53475e2886d1f88a9a24c38339d9df68279e81e9
Coconuts-del <6712@holbertonstudents.com>
2024-01-03 01:12:17 +0100

    add notes

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
-# concat 2 strings with typing variable annotation
+""" concat 2 strings with typing variable annotation """
+
+
 def concat(str1: str, str2: str) -> str:
+    """ concat str1 and str2 """
     return (str1 + str2)
