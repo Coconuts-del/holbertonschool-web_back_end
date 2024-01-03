97f9fb08cc54140b94c6767a0dba58a14cd1d99e
Coconuts-del <6712@holbertonstudents.com>
2024-01-03 17:22:06 +0100

    add typing annotated to delays

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """ execute multiple coroutines at same time with async """
 import asyncio
-import random
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
@@ -9,7 +8,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """ call n times wait_random """
     coroutines = [wait_random(max_delay) for time in range(n)]
-    delays = []
+    delays: List[float] = []
 
     for coroutine in asyncio.as_completed(coroutines):
         delay = await coroutine
