524a8ceb858ab06aa53e5db071624e6c45969a64
Coconuts-del <6712@holbertonstudents.com>
2024-01-03 01:08:48 +0100

    add note

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
-# add 2 floats with typing variable annotation
+""" add 2 floats a b with typing variable annotation """
+
+
 def add(a: float, b: float) -> float:
+    """ return sum of a and b """
     return (a + b)
