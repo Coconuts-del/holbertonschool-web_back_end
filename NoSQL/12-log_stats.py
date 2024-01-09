d045a8a4195fd783a3b169a974dfe14c3aac994d
Coconuts-del <6712@holbertonstudents.com>
2024-01-09 19:18:02 +0100

    add //127..

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -5,10 +5,9 @@ from pymongo import MongoClient
 
 def main():
     # Connect to MongoDB
-    client = MongoClient()
+    client = MongoClient('mongodb://127.0.0.1:27017')
     db = client.logs
     collection = db.nginx
-
     # Count total number of logs
     total_logs = collection.count_documents({})
     print(f"{total_logs} logs")
