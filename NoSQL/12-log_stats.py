e37d9201e9759a2be547cf4ee46f27e39fc89fd2
Coconuts-del <6712@holbertonstudents.com>
2024-01-10 14:22:11 +0100

    use dictionary method.counts

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -4,20 +4,21 @@ from pymongo import MongoClient
 
 
 def main():
-    # Connect to MongoDB
+    """ Connect to MongoDB
+        Count total number of logs
+        Count logs for each HTTP method
+        Count logs with method=GET and path=/status """
     client = MongoClient('mongodb://127.0.0.1:27017')
     db = client.logs
     collection = db.nginx
-    # Count total number of logs
     total_logs = collection.count_documents({})
     print(f"{total_logs} logs")
     print("Methods:")
-    # Count logs for each HTTP method
     http_methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    method_counts = {}
     for method in http_methods:
-        count = collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {count}")
-    # Count logs with method=GET and path=/status
+        method_counts[method] = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {method_counts[method]}")
     status_check_count = collection.count_documents(
                          {"method": "GET", "path": "/status"})
     print(f"{status_check_count} status check")
