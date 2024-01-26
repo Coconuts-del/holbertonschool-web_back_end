b1eb8d768511471eff4f287452f8a8574f0e8ed0
Coconuts-del <6712@holbertonstudents.com>
2024-01-26 12:36:44 +0100

    add status

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,5 +2,14 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
-  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)]);
+  return Promise.allSettled([
+    uploadPhoto(fileName).then(
+      (value) => ({ status: 'fulfilled', value }),
+      (reason) => ({ status: 'rejected', reason }),
+    ),
+    signUpUser(firstName, lastName).then(
+      (value) => ({ status: 'fulfilled', value }),
+      (reason) => ({ status: 'rejected', reason }),
+    ),
+  ]);
 }
