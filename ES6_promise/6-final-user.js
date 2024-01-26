d50e8dcfbf7392c61994aff3c06dc796a28a0f08
Coconuts-del <6712@holbertonstudents.com>
2024-01-26 13:53:10 +0100

    using map

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,15 +1,10 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
-  return Promise.allSettled([
-    uploadPhoto(fileName).then((result) => ({
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)])
+    .then((results) => (results.map((result) => ({
       status: result.status,
-      value: result.status === 'fulfilled' ? result.value : String(result.reason),
-    })),
-    signUpUser(firstName, lastName).then((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : String(result.reason),
-    })),
-  ]);
+      value: result.value ? result.value : String(result.reason),
+    }))));
 }
